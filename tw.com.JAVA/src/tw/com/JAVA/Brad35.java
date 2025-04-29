b621a435312b14a1eb86aa623cb0fc94f8e83cc4
Alice <kookqq02@gmail.com>
2025-04-29 09:12:33 +0800

    202504290900

---
+++ tw.com.JAVA/src/tw/com/JAVA/Brad35.java
@@ -0,0 +1,18 @@
+package tw.com.JAVA;
+
+import java.util.LinkedList;
+
+public class Brad35 {
+
+	public static void main(String[] args) {
+		LinkedList<String> names = new LinkedList<String>();
+		names.add(0,"Brad");	// 0
+		names.add(0,"Eric");	// 1
+		names.add(0,"Andy");	// 2
+		names.add(0,"Mark");
+		names.add(0,"John");
+		System.out.println(names.size());
+		System.out.println(names);
+	}
+
+}
